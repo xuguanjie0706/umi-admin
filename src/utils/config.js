94c11f40d7577331a3c1096fe51857a344814733
xuguanjie <xuguanjie@halove.com>
2022-09-03 07:05:52 +0800

    地址修改

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,13 @@
 /*
  * @Author: xgj
  * @since: 2022-08-30 20:25:00
- * @lastTime: 2022-09-01 01:04:35
+ * @lastTime: 2022-09-03 07:05:44
  * @LastAuthor: xgj
  * @FilePath: /umi-admin/src/utils/config.js
  * @message: 
  */
 export default {
-  url: process.env.NODE_ENV === 'development' ? 'http://localhost:7001' : 'http://pick.yystart.com',
+  url: process.env.NODE_ENV === 'development' ? 'http://localhost:7001' : 'http://124.222.170.81',
   webUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8001/#/Exchange/' : 'http://pick.yystart.com/mobile/#/Exchange/',
-  url: process.env.NODE_ENV === 'development' ? 'http://localhost:7001/' : 'http://localhost:8000/'
+  url: process.env.NODE_ENV === 'development' ? 'http://localhost:7001/' : 'http://124.222.170.81/'
 };
